c9fee2885a9b125fde4d88b5223e421cf3a3ea28
Dante291 <gparag222@gmail.com>
2023-07-28 21:32:55 +0530

    state management

--- lib/Screens/product_detail_screen.dart
+++ lib/Screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/Providers/products.dart';
 
 class productDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
@@ -6,6 +8,10 @@ class productDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productID = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProducts = Provider.of<Products>(context, listen: false)
+        .items
+        .firstWhere((element) => element.id == productID);
     return const Placeholder();
   }
 }
