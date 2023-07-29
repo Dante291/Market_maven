9b84acd73eaefe6917568e8e3dbd0f225b7c2ab8
Dante291 <gparag222@gmail.com>
2023-07-29 19:00:46 +0530

    state management

--- lib/Screens/product_overview_screen.dart
+++ lib/Screens/product_overview_screen.dart
@@ -2,14 +2,49 @@ import 'package:flutter/material.dart';
 
 import '../Widgets/product_grid.dart';
 
-class productOverviewScreen extends StatelessWidget {
+enum filterOptions {
+  Favourites,
+  All,
+}
+
+class productOverviewScreen extends StatefulWidget {
+  @override
+  State<productOverviewScreen> createState() => _productOverviewScreenState();
+}
+
+class _productOverviewScreenState extends State<productOverviewScreen> {
+  var _showOnlyFav = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Shop App'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (filterOptions selectedvalue) {
+              setState(() {
+                if (selectedvalue == filterOptions.Favourites) {
+                  _showOnlyFav = true;
+                } else {
+                  _showOnlyFav = false;
+                }
+              });
+            },
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: filterOptions.Favourites,
+                child: Text('Only favourites Products'),
+              ),
+              const PopupMenuItem(
+                value: filterOptions.All,
+                child: Text('All Products'),
+              ),
+            ],
+            icon: const Icon(Icons.more_vert),
+          )
+        ],
       ),
-      body: ProductGrid(),
+      body: ProductGrid(_showOnlyFav),
     );
   }
 }
