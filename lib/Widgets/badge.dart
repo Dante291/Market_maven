8400462bacd87af9f9f55dabf8e3f0951f12014f
Dante291 <gparag222@gmail.com>
2023-08-07 19:41:51 +0530

    orders

--- lib/Widgets/badge.dart
+++ lib/Widgets/badge.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Screens/cart_screen.dart';
 
 class badge extends StatelessWidget {
   const badge({
@@ -19,22 +20,25 @@ class badge extends StatelessWidget {
         Positioned(
           right: 10,
           top: 8,
-          child: Container(
-            padding: EdgeInsets.all(2.0),
-            // color: Theme.of(context).accentColor,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: color,
-            ),
-            constraints: BoxConstraints(
-              minWidth: 2,
-              minHeight: 2,
-            ),
-            child: Text(
-              value,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 15,
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).pushNamed(cartScreen.routename),
+            child: Container(
+              padding: EdgeInsets.all(2.0),
+              // color: Theme.of(context).accentColor,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: color,
+              ),
+              constraints: BoxConstraints(
+                minWidth: 2,
+                minHeight: 2,
+              ),
+              child: Text(
+                value,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
               ),
             ),
           ),
