9d96edf14df1f262f34c190f4428ad90f7eb7214
Dante291 <gparag222@gmail.com>
2023-08-22 16:20:26 +0530

    add products

---
+++ lib/Screens/user_product_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class UserProductScreen extends StatelessWidget {
+  const UserProductScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: ),
+    )
+  }
+}
