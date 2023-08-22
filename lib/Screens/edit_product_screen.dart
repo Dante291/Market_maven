0fa439e95c7c3eb91ba83dd81b806325734e59f1
Dante291 <gparag222@gmail.com>
2023-08-22 19:28:03 +0530

    user inupt

--- lib/Screens/edit_product_screen.dart
+++ lib/Screens/edit_product_screen.dart
@@ -10,34 +10,44 @@ class EditproductScreen extends StatefulWidget {
 
 class _EditproductScreenState extends State<EditproductScreen> {
   final FocusNode _focusNode = FocusNode();
+  final FocusNode _pricefocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        // Unfocus all focus nodes
-        FocusScope.of(context).requestFocus(new FocusNode());
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Edit Product'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            behavior: HitTestBehavior.opaque,
             child: ListView(
               children: [
                 GestureDetector(
-                  onTap: () {
-                    // Prevent the GestureDetector above from unfocusing this field
-                    _focusNode.requestFocus();
-                  },
+                  onTap: () => _focusNode.requestFocus(_focusNode),
                   child: TextFormField(
                     focusNode: _focusNode,
                     decoration: InputDecoration(labelText: 'Title'),
                     textInputAction: TextInputAction.next,
+                    onFieldSubmitted: (value) {
+                      _pricefocusNode.requestFocus();
+                    },
                   ),
-                )
+                ),
+                GestureDetector(
+                  onTap: () => _pricefocusNode.requestFocus(),
+                  child: TextFormField(
+                    focusNode: _pricefocusNode,
+                    decoration: InputDecoration(labelText: 'Price'),
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
               ],
             ),
           ),
