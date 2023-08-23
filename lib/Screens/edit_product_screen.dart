ef7d6d127302b93481292911591afd031d2500d5
Dante291 <gparag222@gmail.com>
2023-08-23 16:51:37 +0530

    taking form user input

--- lib/Screens/edit_product_screen.dart
+++ lib/Screens/edit_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/Providers/Product.dart';
 
 class EditproductScreen extends StatefulWidget {
   static const routename = '/edit_product';
@@ -14,6 +15,8 @@ class _EditproductScreenState extends State<EditproductScreen> {
   final FocusNode _descriptionfocusNode = FocusNode();
   final imageURLcontroller = TextEditingController();
   final FocusNode _imagefocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+  var _editedproduct = Product(id: '', imageUrl: '', price: 0, title: '');
 
   @override
   void dispose() {
@@ -38,15 +41,30 @@ class _EditproductScreenState extends State<EditproductScreen> {
     }
   }
 
+  void _saveform() {
+    _form.currentState?.save();
+    print(_editedproduct.id);
+    print(_editedproduct.price);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: [
+          IconButton(
+              onPressed: _saveform,
+              icon: Icon(
+                Icons.save,
+                size: 30,
+              ))
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _form,
           child: GestureDetector(
             onTap: () {
               FocusScope.of(context).unfocus();
@@ -63,6 +81,15 @@ class _EditproductScreenState extends State<EditproductScreen> {
                     onFieldSubmitted: (value) {
                       _pricefocusNode.requestFocus();
                     },
+                    onSaved: (newValue) {
+                      if (newValue != null) {
+                        _editedproduct = Product(
+                            id: _editedproduct.id,
+                            title: newValue,
+                            imageUrl: _editedproduct.imageUrl,
+                            price: _editedproduct.price);
+                      }
+                    },
                   ),
                 ),
                 GestureDetector(
@@ -75,15 +102,15 @@ class _EditproductScreenState extends State<EditproductScreen> {
                     onFieldSubmitted: (value) {
                       _descriptionfocusNode.requestFocus();
                     },
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => _descriptionfocusNode.requestFocus(),
-                  child: TextFormField(
-                    focusNode: _descriptionfocusNode,
-                    decoration: InputDecoration(labelText: 'Description'),
-                    maxLines: 3,
-                    keyboardType: TextInputType.multiline,
+                    onSaved: (newValue) {
+                      if (newValue != null) {
+                        _editedproduct = Product(
+                            id: _editedproduct.id,
+                            title: newValue,
+                            imageUrl: _editedproduct.imageUrl,
+                            price: double.parse(newValue));
+                      }
+                    },
                   ),
                 ),
                 Row(
@@ -108,6 +135,15 @@ class _EditproductScreenState extends State<EditproductScreen> {
                         textInputAction: TextInputAction.done,
                         controller: imageURLcontroller,
                         focusNode: _imagefocusNode,
+                        onSaved: (newValue) {
+                          if (newValue != null) {
+                            _editedproduct = Product(
+                                id: _editedproduct.id,
+                                title: newValue,
+                                imageUrl: newValue,
+                                price: _editedproduct.price);
+                          }
+                        },
                       ),
                     ),
                   ],
